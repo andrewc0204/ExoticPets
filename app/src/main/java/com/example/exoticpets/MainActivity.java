ff443f49ac855a7b317c7b88755c8ae48f04323f
Andrew <andrewc0204@gmail.com>
2021-06-13 23:12:47 -0600

    Background, Calendar
    Added color to background, and then added the calendar

--- app/src/main/java/com/example/exoticpets/MainActivity.java
+++ app/src/main/java/com/example/exoticpets/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 //Hello
 
@@ -30,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         myList = new String[]{"Wally", "Chinnok"};
         ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myList);
         myPets.setAdapter(myarrayAdapter);
+        myPets.setBackgroundColor(Color.parseColor("#0E86D4"));
+        final EditText input = new EditText(this);
+
+
 
         plus = findViewById(R.id.addPet);
         plus.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 new AlertDialog.Builder(MainActivity.this)
                         .setTitle("Enter Pet Name")
                         .setMessage("Enter your pet name!")
+
                         // Specifying a listener allows you to take an action before dismissing the dialog.
                         // The dialog is automatically dismissed when a dialog button is clicked.
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
@@ -46,11 +54,12 @@ public class MainActivity extends AppCompatActivity {
                             }
                         })
 
+
                         // A null listener allows the button to dismiss the dialog and take no further action.
                         .setNegativeButton(android.R.string.no, null)
                         .show();
 
-//                Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
             }
         });
         
