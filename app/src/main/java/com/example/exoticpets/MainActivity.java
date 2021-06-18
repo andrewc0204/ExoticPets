01f012c5466719f9dc98649545c68349104891f3
Andrew <andrewc0204@gmail.com>
2021-06-17 22:35:59 -0600

    Add Pet
    Added ability to add pet to list view

--- app/src/main/java/com/example/exoticpets/MainActivity.java
+++ app/src/main/java/com/example/exoticpets/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 //Hello
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     ListView myPets;
     String[] myList;
     Button plus;
+    ArrayList<String> arrayNames = new ArrayList<String>();
 
 
     @Override
@@ -31,11 +34,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myPets = findViewById(R.id.myPets);
+        plus = findViewById(R.id.addPet);
+
         myList = new String[]{"Wally", "Chinnok"};
-        ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myList);
+        ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayNames);
         myPets.setAdapter(myarrayAdapter);
         myPets.setBackgroundColor(Color.parseColor("#0E86D4"));
-        plus = findViewById(R.id.addPet);
+
 
         final EditText input = new EditText(this);
         plus.setOnClickListener(new View.OnClickListener() {
@@ -43,12 +48,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 new AlertDialog.Builder(MainActivity.this)
                         .setTitle("Enter Pet Name")
-                        //.setMessage("Enter your pet name!")
                         .setView(input)
                         // Specifying a listener allows you to take an action before dismissing the dialog.
+
                         // The dialog is automatically dismissed when a dialog button is clicked.
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
+                              myarrayAdapter.add(input.getText().toString());
+                              myPets.deferNotifyDataSetChanged();
+                                if(input.getParent()!=null)
+                                    ((ViewGroup)input.getParent()).removeView(input);
                             }
                         })
                         // A null listener allows the button to dismiss the dialog and take no further action.
